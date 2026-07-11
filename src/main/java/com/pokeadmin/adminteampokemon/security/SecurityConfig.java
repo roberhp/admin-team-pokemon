a527b85651aa4d5d8494acbbc4dc9c087ca98493
Roberto Huerta <robertoescom@hotmail.com>
2026-07-10 19:37:41 -0600

    Modification to get actuator with health and info

--- src/main/java/com/pokeadmin/adminteampokemon/security/SecurityConfig.java
+++ src/main/java/com/pokeadmin/adminteampokemon/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,20 +22,20 @@ public class SecurityConfig {
                         HttpSecurity http) throws Exception {
 
                 return http.csrf(csrf -> csrf.disable())
+                                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                 .authorizeHttpRequests(auth -> auth
                                         .requestMatchers(
-                                                "/api/v1/auth/**",
-                                                "/swagger-ui/**",
-                                                "/v3/api-docs/**",
-                                                "/api/pokedex/**"
-                                                )
-                                        .permitAll()
-                                        .anyRequest()
-                                        .authenticated())
+                                        "/api/v1/auth/**",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/api/pokedex/**",
+                                        "/actuator/health",
+                                        "/actuator/info"
+                                        ).permitAll()
+                                        .anyRequest().authenticated())
                                 .addFilterBefore(
-                                                jwtAuthenticationFilter,
-                                                UsernamePasswordAuthenticationFilter.class)
-                                .httpBasic(Customizer.withDefaults())
+                                        jwtAuthenticationFilter,
+                                        UsernamePasswordAuthenticationFilter.class)
                                 .build();
         }
 }
